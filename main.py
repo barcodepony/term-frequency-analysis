4441c493aa5cada52c7e9f504848aeee3923f720
Michael Gombocz <michael.gombocz@student.tugraz.at>
2018-12-05 00:38:08 +0100

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import preprocessing
+
+X = [["hallo","wie","geht","es","dir","denn","so"],
+     ["alle","meine","freunde","geht","es","gut"],
+     ["das","ist","der","letze","test","satz"],
+     ["alle", "meine", "freunde", "geht", "es", "gut"]
+     ]
+
+N = 4
+k=6
+
+def test(X=X, N=N, k=k):
+    preprocessing.preprocess(X, N, k)
+
+if __name__ == "__main__":
+    test()
---
+++ preprocessing.py
@@ -0,0 +1,43 @@
+def bubbleSort(c, y):
+    for value in range(len(c)-1,0,-1):
+        for i in range(value):
+            if c[i]<c[i+1]:
+                temp = c[i]
+                temp_y = y[i]
+                c[i] = c[i+1]
+                y[i] = y[i+1]
+                c[i+1] = temp
+                y[i+1] = temp_y
+    return(c, y)
+
+def preprocess(X, n, k):
+    assert isinstance(X, list)
+    assert len(X) == n
+    assert isinstance(k, int)
+
+    y = []  # list of text fragments
+    c = []  # list of amounts
+
+    ix = 0
+    ixmax = len(X)-1
+    for r in  X:
+        i = 0
+        mi = len(r)-1
+        while(i <= mi-(k-1)):
+            fragment = r[i:i+k]
+            if fragment in y:
+                c_i = y.index(fragment)
+                c[c_i] = c[c_i] + 1
+            else:
+                y.append(fragment)
+                c.append(1)
+            i = i + 1
+
+    print(y)
+    print(c)
+
+    c, y = bubbleSort(c, y)
+    print("fin")
+
+
+
